279d127a018411bf6d2370fa35597ee5d6b26bdc
Feng Li <m@feng.li>
2023-02-27 15:20:41 +0800

    Add ido ignore

--- init.el
+++ init.el
@@ -592,7 +592,8 @@
           "^\\*Ibuffer*" "^\\*ESS-errors*" "^\\*Warnings*" "*TeX Help*"
           "*Pymacs*" "*Flymake log*" "\\.log$" "^\\*.*Completions\\*$" "^\\*Ediff" "^\\*tramp"
           "^\\*cvs-" "_region_" "^TAGS$" "^\\*Ido" "^\\*.*dictem buffer\\*$"
-          "^\\*inferior-lisp*" "^\\*Compile-Log\\*" "*output*" "\.\*output*"))
+          "^\\*inferior-lisp*" "^\\*Compile-Log\\*" "*output*" "\.\*output*"
+          "^\\*EGLOT*" ))
 
   (defun ido-kill-emacs-hook () (ignore-errors (ido-save-history)))
   )
