da7a1a7731c7148cbdcf780e60fcaa0d979db608
Feng Li <m@feng.li>
2024-02-08 22:11:51 +0800

    Update eln-cache

--- early-init.el
+++ early-init.el
@@ -1,6 +1,12 @@
 (setq package-enable-at-startup t)
 (setq package-user-dir (concat (getenv "HOME") "/.config/emacs/elpa")) ;; Global
 
+;; Set eln-cache dir
+(when (boundp 'native-comp-eln-load-path)
+  (startup-redirect-eln-cache (expand-file-name
+                               (concat (getenv "HOME") "/.config/emacs/eln-cache")
+                               user-emacs-directory)))
+
 ;; Environment variables
 (setenv "OMP_NUM_THREADS" "1")
 (setenv "LSP_USE_PLISTS"  "true")
